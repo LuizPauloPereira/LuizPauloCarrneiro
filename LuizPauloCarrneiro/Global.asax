c489f31d1ba6a63cdf065ead009588d3b45148e4
Luiz Paulo Pereira <luizpaulopereiraf@gmail.com>
2019-05-26 22:51:58 -0400

    Adicionar arquivos de projeto.

---
+++ LuizPauloCarrneiro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LuizPauloCarrneiro.MvcApplication" Language="C#" %>
---
+++ LuizPauloCarrneiro/MetaDataType/AULA_MD_MetaDataType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LuizPauloCarrneiro.Models
+{
+    [MetadataType(typeof(AULA_MD_MetaDataType))]
+    public partial class AULA_MD { }
+
+
+    public partial class AULA_MD_MetaDataType
+    {
+        [DisplayName("Código da aula")]
+        [DataType(DataType.Custom)]
+        public int AulaId { get; set; }
+
+        [DisplayName("Descrição da aula")]
+        [Required(ErrorMessage = "este campo é obrigatório")]
+        [StringLength(50, MinimumLength =10)]
+        public string AulaDescricao { get; set; }
+    }
+}
